ce5f48257023378870409ac8d92c616330c2051c
Raunak Kundu <106420048+Raunak-Kundu@users.noreply.github.com>
2024-04-08 16:31:21 +0530

    Add files via upload

--- 3-DigitalClock/script.js
+++ 3-DigitalClock/script.js
@@ -1,1 +1,7 @@
-console.log("Hello!!")
+const clock= document.querySelector('#clock')
+
+setInterval(function(){
+    let date = new Date();
+    clock.innerHTML = date.toLocaleTimeString();
+
+}, 1000);
